f430be1f5ecf4cc129a58657269f4ba803300b36
Dz6k <pedrosouzadz6@pm.me>
2024-03-14 05:09:30 -0300

    update files

--- main.py
+++ main.py
@@ -1,53 +1,23 @@
 from script import main
 import customtkinter as tk
 
-
-
-import customtkinter as ctk
-from customtkinter import CTkImage
-from PIL import ImageTk, Image
-
-# Create an instance of tkinter window
-win = ctk.CTk()
-win.resizable(0,0)
-# Define the geometry of the window
-win.geometry("300x300")
-win.title('Donate pix')
-frame = ctk.CTkFrame(win, width=300, height=300)
-frame.pack()
-frame.place(anchor='center', relx=0.5, rely=0.5)
-
-# Create an object of tkinter ImageTk
-img = Image.open("pix.png")
-img = CTkImage(light_image=Image.open("pix.png"),
-               dark_image=Image.open("pix.png"),
-               size=(295, 295))
-
-# Create a Label Widget to display the text or Image
-label = ctk.CTkLabel(frame, text=None, image=img)
-label.pack()
-
-win.mainloop()
-
-
 def script_gui():
-    # visual
+
     tk.set_appearance_mode('dark')
     tk.set_default_color_theme('dark-blue')
-    # definindo variavel
     root = tk.CTk()
-    # remove opcao "maximizar" do windows
+
     root.resizable(0,0)
-    # titulo da janela 
+
     root.title('[Discord: dz6k]')
-    # dimensao
+
     root.geometry('250x100')
     tk.set_widget_scaling(1.1)
 
     frame = tk.CTkFrame(master=root)
     frame.pack(pady=10,padx=10,fill='both',expand=True)
     
-    # modos
+
     button_stealth_farm = tk.CTkButton(master=frame,text='Start', command=main)
     button_stealth_farm.pack(padx=12,pady=10)
 
--- script.py
+++ script.py
@@ -1,33 +1,23 @@
-import win32con
-import pygetwindow as gw
-from threading import Thread 
-from win32gui import SendMessage
-from random import randint, choice
-from mousekey import MouseKey
-from time import sleep
-import pyautogui
+import win32con  # this lib was used to send command key up and key down, like when you press any key
+import pygetwindow as gw  # this lib was used to get the handle by aplication's title
+from threading import Thread  # this lib was used to create a thread
+from win32gui import SendMessage  # This lib was used to send keystrokes
+from random import randint, choice # this lib was used to generate a random numbers in "loop for" and choose one random value, respectively
+from mousekey import MouseKey # this lib was used to make a safe stop key(ctrl+e)
+from time import sleep  # this lib was used to pause for a while in seconds.... I don't know of any other simpler way to explain what is already simple
+import pyautogui  # this lib was used to and just to send a spam message if you try execute the script more than once
+
 
 # variables
+POSSIBILITIES = [3.5,
+                 3.8,
+                 4,
+                 4.5,
+                 4.8,
+                 5]
+execution = 0
 
-POSSIBILITIES = [2,
-                 2.05,
-                 2.10,
-                 2.15,
-                 2.20,
-                 1.95,
-                 2.25,
-                 2.30,
-                 2.5,
-                 2.60,
-                 2.75,
-                 2.95,
-                 3,
-                 3.2,
-                 3.5,
-                 3.6]
-contador = 0  
-
-# pega o handle da janela 
+# Take the handle based on windows title
 def handle_of_windowtitle(title):
     try:
         window = gw.getWindowsWithTitle(title)[0]
@@ -35,53 +25,48 @@ def handle_of_windowtitle(title):
     except IndexError:
         return None
   
-#  funcao de iniciar farm  
-def stealthfarm():
-    
+  
+# the script function
+def farm():
+    # key for safe stop
     mkey = MouseKey()
     mkey.enable_failsafekill('ctrl+e')
-    # cria o handle da janela alvo
+    # take handle of apliction
     hwnd =  handle_of_windowtitle("NIGHT CROWS(1)")
     
-    #  envia input Q no hexadecimal 0x51
-    SendMessage(hwnd, win32con.WM_KEYDOWN, 0x51 , 0)
+    # send input Q (0x51) - start auto atack
+    SendMessage(hwnd, win32con.WM_KEYDOWN, 0x51 , None)
     sleep(0.01)
     SendMessage(hwnd, win32con.WM_KEYUP, 0x51 , 0)
     sleep(0.2)
 
-    
+    # "infinite" loop for send inputs for a long long time....
     while True:
-        #  NECESSITA FAZER UMA CONFIGURACAO ANTES DENTRO DO JOGOw
-        
-        # abre o "procurador de mob"
-        # enviar input U no hexadecimal 0x55 
+        #  YOU NEED TO MAKE AN IN-GAME SETUP
+
+        # send input U (ux55)
         SendMessage(hwnd,win32con.WM_KEYDOWN, 0x55 , 0)
         sleep(0.01)
         SendMessage(hwnd,win32con.WM_KEYUP, 0x55 , 0)
         
-        # laco para iterar sobre os alvos
+        # Loop to iterate on the mob
         for i in range(randint(2,4)):
-            #  enviar input TAB no hexadecimla 0x09
+            #  send input TAB (ux09)
             SendMessage(hwnd,win32con.WM_KEYDOWN, 0x09 , 0)
             SendMessage(hwnd,win32con.WM_KEYUP, 0x09 , 0)
             sleep(0.2)
-        
+        # sleep loop for random some time
         sleep(choice(POSSIBILITIES))
-    # caso use a funcao stop threads
-    #  envia input Q no hexadecimal 0x51
-    SendMessage(hwnd, win32con.WM_KEYDOWN, 0x51 , 0)
-    sleep(0.01)
-    SendMessage(hwnd, win32con.WM_KEYUP, 0x51 , 0)
-    sleep(0.2) 
        
 def main(): 
-    global contador
-    contador += 1
-    if contador > 1:
-        pyautogui.alert(title='Acao Bloqueada!',text='Voce ja inicou esta funcao')
+    global execution
+    execution += 1
+    if execution > 1:
+        pyautogui.alert(title='Action denied!',
+                        text='You have already started this function')
+    # Create thread
     else:        
-        # criando threads das funcoes    
-        # thread que inica o script
-        start_farm = Thread(target=stealthfarm)
-        # inicia thread do script
+        # this line creat a thread of funcion farm
+        start_farm = Thread(target=farm)
+        # and this line starts it
         start_farm.start()
